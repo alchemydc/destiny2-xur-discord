9f4d247018f7aa25cfad8164ab19269631aa0f35
DC <dc@technoexile.com>
2021-02-12 20:34:58 -0700

    workaround env issues in shell wrapper

--- xur.sh
+++ xur.sh
@@ -2,4 +2,5 @@
 set -x
 
 source /home/xur/destiny2-xur-discord/bin/activate
-/home/xur/destiny2-xur-discord/bin/python3 /home/xur/destiny2-xur-discord/xur.py
+cd /home/xur/destiny2-xur-discord/xur.py
+./xur.py
